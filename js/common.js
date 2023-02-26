e7418f2d6ac4a4416cca2f80e86c5da90897b453
Neotwalker <and1marat9@gmail.com>
2023-02-26 19:32:09 +0500

    add

--- js/common.js
+++ js/common.js
@@ -1,6 +1,28 @@
 $(function() {
 
+	$("#phone_1, #phone_2").mask("+7 (999) 999-9999");
+	
 	let rellax = new Rellax('.rellax');
+
+	$('.burger').click(function(){
+		$(this).toggleClass('open');
+		if ($(this).hasClass('open')) {
+			$('.header--menu').fadeIn().css('display', 'flex');
+		} else {
+			$('.header--menu').fadeOut();
+
+		}
+	});
+
+	const about__button = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('fade-in-left-2');
+			}
+		});
+	});
+
+	about__button.observe(document.querySelector('.main--about .button'));
 
 	const observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
@@ -57,8 +79,17 @@ $(function() {
 		],
 		mouseDrag: false,
 		smartSpeed: 750,
-		// autoHeight: true,
-		loop: true
+		loop: true,
+		responsive: {
+			0: {
+				autoHeight: true,
+				margin: 30
+			},
+			769: {
+				autoHeight: false,
+				margin: 0
+			}
+		},
 	});
 
 	$('.faq--item').click(function(){
@@ -73,5 +104,101 @@ $(function() {
 		}
 		$a.slideToggle();
 	});
+
+	if ($(window).width() >= '995'){
+		let bg = document.querySelector('.main--form .icon');
+		let rotate = document.querySelector('.main--form .rotate');
+		window.addEventListener('mousemove', function(e) {
+			let x = e.clientX / window.innerWidth;
+			let y = e.clientY / window.innerHeight;  
+			bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+			rotate.style.transform = 'rotate('+y*x*100+'deg)';
+		});
+	} else {
+		$('.main--form .wrapper .icon-1').addClass('watch-animation');
+		$('.main--form .wrapper .icon-3').addClass('watch-animation');
+		const form__cube = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('rotate-in');
+				}
+			});
+		});
+	
+		form__cube.observe(document.querySelector('.main--form .wrapper .icon-1'));
+		form__cube.observe(document.querySelector('.main--form .wrapper .icon-3'));
+	}
+
+	if ($(window).width() >= '995'){
+		const form__arrow = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('fade-in-left-2');
+				}
+			});
+		});
+
+		form__arrow.observe(document.querySelector('.main--form .wrapper--left .text .icon'));
+	} else {
+		const form__arrow = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('pulse-in-4');
+				}
+			});
+		});
+
+		form__arrow.observe(document.querySelector('.main--form .wrapper--left .text .icon'));
+	}
+
+	const why = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('fade-in-left-2');
+			}
+		});
+	});
+
+	why.observe(document.querySelector('.main--why .button'));
+
+	const why1 = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('fade-in-right-2');
+			}
+		});
+	});
+
+	why1.observe(document.querySelector('.main--why .button--white'));
+
+	const footer__logo = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('slide-from-bot');
+			}
+		});
+	});
+
+	footer__logo.observe(document.querySelector('.footer .logo img'));
+
+	const footer__menu_left = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('fade-in-left-2');
+			}
+		});
+	});
+
+	footer__menu_left.observe(document.querySelector('.footer--menu'));
+
+	const footer__menu_right = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('fade-in-right-2');
+			}
+		});
+	});
+
+	footer__menu_right.observe(document.querySelector('.footer--menu.right'));
 
 });
